efdfb63d96e1a15bd5709a965901cbceec8a81a8
ndeimler99 <nathanieldeimler@gmail.com>
2025-03-19 11:38:27 +0000

    modified alignment plots to stop throwing errors

--- bin/alignment_plots.R
+++ bin/alignment_plots.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 # script funtions to generate summary stats of entire sequencing run/experiment
 args = commandArgs(trailingOnly=TRUE)
-data_table <- read.table(args[0], sep="\t", header=TRUE)
+data_table <- read.table(args[1], sep="\t", header=TRUE)
 data_table <- reshape2::melt(data_table)
 data_table <- data_table[data_table$variable=="Perc_Nucl",]
 aln_plot <- ggplot(data=data_table) +
@@ -16,7 +16,7 @@ aln_plot <- ggplot(data=data_table) +
   ylab("Percentage of Reads") +
   theme(axis.title.x = element_blank(),
         axis.title.y=element_text(size=20),
-        axis.legend.title = element_text(size=15),
+        legend.title = element_text(size=15),
         axis.text=element_text(size=15))
 
 ggsave("alignment_distribution.pdf", device="pdf", width=4, height=12, plot=aln_plot)
